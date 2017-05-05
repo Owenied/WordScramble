161b101881f351cf4eb5028fee2be4da2d4d7893
Owen Duignan <owenied@me.com>
2017-05-05 12:09:50 -0500

    Add a file with c. 12K words. Create arrays to hold all words and used words. Display a title on top.

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Get the path of the start file and load the contents into the allWords array
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        startGame()
+    }
+    
+    // Shuffle all the words in the allWords array and place the first word into the view controllers title
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        //
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    // Make the number of rows equal to the number of rows used
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    // Show and reuse cells
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +58,70 @@ class ViewController: UITableViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
